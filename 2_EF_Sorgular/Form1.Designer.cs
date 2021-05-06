41a86dc127ce0f2361a7e73c6fe02330edb19cdf
seyid <seyid@DESKTOP-NTP7NSG>
2021-05-06 17:59:07 +0300

    first message

---
+++ 2_EF_Sorgular/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace _2_EF_Sorgular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSonuclar = new System.Windows.Forms.DataGridView();
+            this.btnSorgu1 = new System.Windows.Forms.Button();
+            this.btnSorgu2 = new System.Windows.Forms.Button();
+            this.btnSorgu3 = new System.Windows.Forms.Button();
+            this.btnSorgu4 = new System.Windows.Forms.Button();
+            this.btnSorgu5 = new System.Windows.Forms.Button();
+            this.btnSorgu6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSonuclar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSonuclar
+            // 
+            this.dgvSonuclar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSonuclar.Location = new System.Drawing.Point(2, 76);
+            this.dgvSonuclar.Name = "dgvSonuclar";
+            this.dgvSonuclar.RowHeadersWidth = 51;
+            this.dgvSonuclar.RowTemplate.Height = 24;
+            this.dgvSonuclar.Size = new System.Drawing.Size(1012, 615);
+            this.dgvSonuclar.TabIndex = 0;
+            // 
+            // btnSorgu1
+            // 
+            this.btnSorgu1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSorgu1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSorgu1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSorgu1.Location = new System.Drawing.Point(12, 12);
+            this.btnSorgu1.Name = "btnSorgu1";
+            this.btnSorgu1.Size = new System.Drawing.Size(162, 58);
+            this.btnSorgu1.TabIndex = 1;
+            this.btnSorgu1.Text = "SORGU 1";
+            this.btnSorgu1.UseVisualStyleBackColor = false;
+            this.btnSorgu1.Click += new System.EventHandler(this.btnSorgu1_Click);
+            // 
+            // btnSorgu2
+            // 
+            this.btnSorgu2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSorgu2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSorgu2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSorgu2.Location = new System.Drawing.Point(180, 12);
+            this.btnSorgu2.Name = "btnSorgu2";
+            this.btnSorgu2.Size = new System.Drawing.Size(162, 58);
+            this.btnSorgu2.TabIndex = 1;
+            this.btnSorgu2.Text = "SORGU 2";
+            this.btnSorgu2.UseVisualStyleBackColor = false;
+            this.btnSorgu2.Click += new System.EventHandler(this.btnSorgu2_Click);
+            // 
+            // btnSorgu3
+            // 
+            this.btnSorgu3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSorgu3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSorgu3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSorgu3.Location = new System.Drawing.Point(348, 12);
+            this.btnSorgu3.Name = "btnSorgu3";
+            this.btnSorgu3.Size = new System.Drawing.Size(162, 58);
+            this.btnSorgu3.TabIndex = 1;
+            this.btnSorgu3.Text = "SORGU 3";
+            this.btnSorgu3.UseVisualStyleBackColor = false;
+            this.btnSorgu3.Click += new System.EventHandler(this.btnSorgu3_Click);
+            // 
+            // btnSorgu4
+            // 
+            this.btnSorgu4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSorgu4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSorgu4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSorgu4.Location = new System.Drawing.Point(516, 12);
+            this.btnSorgu4.Name = "btnSorgu4";
+            this.btnSorgu4.Size = new System.Drawing.Size(162, 58);
+            this.btnSorgu4.TabIndex = 1;
+            this.btnSorgu4.Text = "SORGU 4";
+            this.btnSorgu4.UseVisualStyleBackColor = false;
+            this.btnSorgu4.Click += new System.EventHandler(this.btnSorgu4_Click);
+            // 
+            // btnSorgu5
+            // 
+            this.btnSorgu5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSorgu5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSorgu5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSorgu5.Location = new System.Drawing.Point(684, 12);
+            this.btnSorgu5.Name = "btnSorgu5";
+            this.btnSorgu5.Size = new System.Drawing.Size(162, 58);
+            this.btnSorgu5.TabIndex = 1;
+            this.btnSorgu5.Text = "SORGU 5";
+            this.btnSorgu5.UseVisualStyleBackColor = false;
+            this.btnSorgu5.Click += new System.EventHandler(this.btnSorgu5_Click);
+            // 
+            // btnSorgu6
+            // 
+            this.btnSorgu6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSorgu6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSorgu6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSorgu6.Location = new System.Drawing.Point(852, 12);
+            this.btnSorgu6.Name = "btnSorgu6";
+            this.btnSorgu6.Size = new System.Drawing.Size(162, 58);
+            this.btnSorgu6.TabIndex = 1;
+            this.btnSorgu6.Text = "SORGU 6";
+            this.btnSorgu6.UseVisualStyleBackColor = false;
+            this.btnSorgu6.Click += new System.EventHandler(this.btnSorgu6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 703);
+            this.Controls.Add(this.btnSorgu6);
+            this.Controls.Add(this.btnSorgu5);
+            this.Controls.Add(this.btnSorgu4);
+            this.Controls.Add(this.btnSorgu3);
+            this.Controls.Add(this.btnSorgu2);
+            this.Controls.Add(this.btnSorgu1);
+            this.Controls.Add(this.dgvSonuclar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSonuclar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSonuclar;
+        private System.Windows.Forms.Button btnSorgu1;
+        private System.Windows.Forms.Button btnSorgu2;
+        private System.Windows.Forms.Button btnSorgu3;
+        private System.Windows.Forms.Button btnSorgu4;
+        private System.Windows.Forms.Button btnSorgu5;
+        private System.Windows.Forms.Button btnSorgu6;
+    }
+}
+
---
+++ 2_EF_Sorgular/Form1.cs
@@ -0,0 +1,212 @@
+﻿using _2_EF_Sorgular.Data;
+using System;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _2_EF_Sorgular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Entities db = new Entities();
+        private void btnSorgu1_Click(object sender, EventArgs e)
+        {
+            #region Soru
+            //Fiyatı 20 ile 50 arasında olan ürünlerin Id ,adı,Fiyatı Miktarı ve Kategori getiren sorgu yaz....
+            //Write a query that returns the Id, Name, Price, Amount and Category of the products whose price is between 20 and 50 ....
+            #endregion
+            #region LING To Entity
+            dgvSonuclar.DataSource = db.Products
+                .Where(p => p.UnitPrice > 20 && p.UnitPrice < 50)
+                .OrderByDescending(p => p.UnitPrice)
+                .Select(p => new
+                {
+                    UrunId = p.ProductID,
+                    UrunAdi = p.ProductName,
+                    p.UnitPrice,
+                    p.UnitsInStock,
+                    p.Category.CategoryName
+                }).ToList();
+            #endregion
+
+            #region Ling To Sql
+            var result = from p in db.Products
+                         where p.UnitPrice > 20 && p.UnitPrice < 50
+                         orderby p.UnitPrice descending
+                         select (new
+                         {
+                             UrunId = p.ProductID,
+                             UrunAdi = p.ProductName,
+                             p.UnitPrice,
+                             p.UnitsInStock,
+                             p.Category.CategoryName
+                         });
+            dgvSonuclar.DataSource = result.ToList();
+
+            #endregion
+        }
+
+        private void btnSorgu2_Click(object sender, EventArgs e)
+        {
+            #region Ling To Entity
+            dgvSonuclar.DataSource = db.Orders.Select(x => new
+            {
+                Musteri = x.Customer.CompanyName,
+                Calisan = x.Employee.FirstName + " " + x.Employee.LastName,
+                SiparişId = x.OrderID,
+                Tarih = x.OrderDate,
+                Kargo = x.Shipper.CompanyName
+            }).ToList();
+            #endregion
+
+            #region LİNG TO Sql
+            var result = from x in db.Orders
+                         select new
+                         {
+                             Musteri = x.Customer.CompanyName,
+                             Calisan = x.Employee.FirstName + " " + x.Employee.LastName,
+                             SiparişId = x.OrderID,
+                             Tarih = x.OrderDate,
+                             Kargo = x.Shipper.CompanyName
+                         };
+            dgvSonuclar.DataSource = result.ToList();
+            #endregion
+
+        }
+
+        private void btnSorgu3_Click(object sender, EventArgs e)
+        {
+            #region Soru
+            //CompanyName icerisinde "restorant" gecen muşterileri listeleyiniz
+            //List the customers who have "restaurant" in Company Name
+            #endregion
+
+            #region LİNG To Entity
+            //dgvSonuclar.DataSource = db.Customers
+            //    .Where(x => x.CompanyName.Contains("restaurant"))
+            //    .ToList();
+            #endregion
+
+            #region LİNG To Sql
+            //var result = from x in db.Customers
+            //             where x.CompanyName.Contains("restaurant")
+            //             select x;
+            //dgvSonuclar.DataSource = result.ToList();
+            #endregion
+
+            #region JOIN
+            var result = from p in db.Products
+                         join c in db.Categories on p.CategoryID equals c.CategoryID
+                         select new
+                         {
+                             p.ProductName,
+                             c.CategoryName
+                         };
+            dgvSonuclar.DataSource = result.ToList();
+            #endregion
+
+
+        }
+
+        private void btnSorgu4_Click(object sender, EventArgs e)
+        {
+            #region Soru
+            //Kategorisi Beverages olan ve ürün adı kola,fiyatı 5.50,Stok Adteti 500 olan ürünü ekleyiniz
+            //Add the product whose category is Beverages and the product name is cola, the price is 5.50, the Stock Quantity is 500.
+            #endregion
+
+            #region 1.Yol
+            int kategoriId = db.Categories
+                .Where(x => x.CategoryName == "Beverages")
+                .FirstOrDefault().CategoryID;
+
+            Product p = new Product();
+            p.ProductName = "Kola 1";
+            p.UnitPrice = 5.5M;
+            p.UnitsInStock = 500;
+            p.CategoryID = kategoriId;
+            db.Products.Add(p);
+            db.SaveChanges();
+
+            //dgvSonuclar.DataSource = db.Products.ToList().OrderByDescending(x => x.ProductID).ToList();
+            #endregion
+
+            #region 2.Yol
+            //db.Products.Add(new Product
+            //{
+            //    ProductName = "Kola 4",
+            //    UnitPrice = 5.75M,
+            //    UnitsInStock = 150,
+            //    CategoryID = db.Categories.FirstOrDefault(x=> x.CategoryName == "Beverages").CategoryID
+            //});
+            //db.SaveChanges();
+
+            //dgvSonuclar.DataSource = db.Products.ToList().OrderByDescending(x => x.ProductID).ToList();
+            #endregion
+
+            #region 3.Yol
+            //db.Categories
+            //    .FirstOrDefault(x => x.CategoryName == "Beverages")
+            //    .Products.Add(new Product
+            //    {
+            //        ProductName = "Kola 5",
+            //        UnitPrice = 6.5M,
+            //        UnitsInStock = 250
+            //    });
+            //db.SaveChanges();
+            //dgvSonuclar.DataSource = db.Products.ToList().OrderByDescending(x => x.ProductID).ToList();
+            #endregion
+
+            dgvSonuclar.DataSource = db.Products.ToList().OrderByDescending(x => x.ProductID).ToList();
+        }
+
+        private void btnSorgu5_Click(object sender, EventArgs e)
+        {
+            #region Soru
+            //Calışanlarık Adını Soyadını Dogum Tarihini ve yaşını getiren sorgu yazınız..
+            //Write a query that brings the Employees' Name, Surname, Date of Birth and Age ..
+            #endregion
+
+            #region LİNG To Entity
+            //dgvSonuclar.DataSource = db.Employees
+            //    .Select(x => new
+            //    {
+            //        x.FirstName,
+            //        x.LastName,
+            //        x.BirthDate,
+            //        Yas = SqlFunctions.DateDiff("YEAR", x.BirthDate, DateTime.Now)
+            //    }).ToList();
+            #endregion
+
+            #region Ling to Sql
+            var result = from x in db.Employees
+                         select new
+                         {
+                             x.FirstName,
+                             x.LastName,
+                             x.BirthDate,
+                             Yas = SqlFunctions.DateDiff("YEAR", x.BirthDate, DateTime.Now)
+                         };
+            dgvSonuclar.DataSource = result.ToList();
+            #endregion
+        }
+
+        private void btnSorgu6_Click(object sender, EventArgs e)
+        {
+            #region LING TO Entity
+            dgvSonuclar.DataSource = db.Products
+                .GroupBy(x => x.Category.CategoryName)
+                .Select(g => new
+                {
+                    KateggoriAdı = g.Key,
+                    ToplamStok = g.Sum(p=> p.UnitsInStock)
+                }).ToList();
+            #endregion
+        }
+    }
+}
